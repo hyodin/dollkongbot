440de41582cbf02a5655374bb659fbe72e95dac5
hyojin <hyojin@yncsmart.com>
2025-10-31 11:48:46 +0900

    프론트, 백엔드 서버 포트 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3005,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false,
         configure: (proxy, _options) => {
