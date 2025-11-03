159801b0c306691046851232f5d4cc93c1918d47
hyojin <hyojin@yncsmart.com>
2025-11-03 12:17:05 +0900

    api proxy 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 3005,
     proxy: {
-      '/api': {
+      '/api/dollkongbot': {
         target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false,
